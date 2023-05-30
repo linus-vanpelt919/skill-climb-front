2fb5a2255e5c312e2810633da659cbf8f4cb1a8e
linus-vanpelt919 <thelastwezardof@gmail.com>
2023-05-30 18:49:23 +0900

    add:mypage修正

--- pages/myPage.tsx
+++ pages/myPage.tsx
@@ -7,23 +7,33 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-interface UserData {
+interface TaskData {
+  id: number;
+  title: string;
+  description: string;
+  category_id: number;
+  category: CategoryData;
+}
+
+const API_URL = "http://skill-climb_laravel.test_1:80/api/myPage";
+
+interface CategoryData {
   id: number;
   name: string;
-  email: string;
 }
-
-const API_URL = "http://skill-climb_laravel.test_1:80/api/show";
 
 interface MyPageProps {
-  user: UserData | null;
+  tasks: TaskData[] | null;
 }
 
-const MyPage: React.FC<MyPageProps> = ({ user }) => {
-  const [userData, setUserData] = React.useState<UserData | null>(user);
+const MyPage: React.FC<MyPageProps> = ({ tasks }) => {
+  const [taskData, setTaskData] = React.useState<TaskData[] | null>(tasks);
 
-  const [email, setEmail] = React.useState(userData?.email || "");
-  const [name, setName] = React.useState(userData?.name || "");
+    // タスクをカテゴリーでグループ化
+    const groupedTasks: Record<string, TaskData[]> | undefined = taskData?.reduce<Record<string, TaskData[]>>((acc, task) => {
+      (acc[task.category_id] = acc[task.category_id] || []).push(task);
+      return acc;
+    }, {});
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen">
@@ -31,34 +41,45 @@ const MyPage: React.FC<MyPageProps> = ({ user }) => {
         <title>SKILL CLIMB</title>
       </Head>
       <Header />
-      <div className="flex flex-col w-full p-8 space-y-4 items-center md:w-1/2 lg:w-1/3">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Profile</h2>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          autoComplete="name"
-          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          autoComplete="email"
-          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-          placeholder="Email address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Update
-        </button>
+      <div className="flex flex-col w-full p-8 space-y-4 items-center lg:w-1/3">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          mypage
+        </h2>
+        {groupedTasks &&
+          Object.entries(groupedTasks).map(([categoryId, tasks]) => (
+            <React.Fragment key={categoryId}>
+              <h3 className="mt-4 text-2xl font-bold text-gray-900">
+                {tasks[0].category.name}
+              </h3>
+              {tasks.map((task: TaskData) => (
+                <div
+                  key={task.id}
+                  className="p-4 mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 w-full"
+                >
+                 <div className="flex-shrink-0">
+                <svg
+                  className="h-12 w-12"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" fill="#2F855A" />
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" fill="#2F855A" />
+                  <path d="M21 19l-9 5-9-5" fill="#2F855A" />
+                  <path d="M3 10l9-5 9 5-9 5-9-5z" fill="#2F855A" />
+                </svg>
+              </div>
+              <div>
+                <div className="text-xl font-medium text-black">
+                  {task.title}
+                </div>
+                <p className="text-gray-500">{task.description}</p>
+              </div>
+                </div>
+              ))}
+            </React.Fragment>
+          ))}
+
       </div>
       <Footer />
     </div>
@@ -72,14 +93,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const response = await axios.get(API_URL, {
       headers: {
-        origin: 'localhost:3000',
+        origin: "localhost:3000",
         cookie: cookie,
       },
     });
-    return { props: { user: response.data } };
+    return { props: { tasks: response.data.tasks } };
   } catch (error) {
-   if (axios.isAxiosError(error) && error.response?.status === 401) {
-
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
       return {
         redirect: {
           destination: "/auth/login",
